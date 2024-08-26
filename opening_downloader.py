3cebaadab69da2985c19894c07fcd2ac775a2eca
TMCooper# <pokemon160377@gmail.com>
2024-08-26 19:35:06 +0200

    Lil update
    remove usless line on import area

--- opening_downloader.py
+++ opening_downloader.py
@@ -5,7 +5,6 @@ import yt_dlp as youtube_dl
 import subprocess
 import os
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import json
 
