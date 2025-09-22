7257b758bfc70edd375f8e4d7144bee5d25c52a3
TMCooper <pokemon160377@gmail.com>
2025-09-22 16:09:11 +0200

    Update

--- launch.bat
+++ launch.bat
@@ -1,12 +1,27 @@
 REM --- EXECUTION ---
 echo [1] Activation de l'environnement virtuel...
-REM L'appel a CALL permet d'executer le script d'activation dans la console actuelle
+REM L'appel a CALL permet d'exécuter le script d'activation dans la console actuelle
 CALL .\op_dl\Scripts\activate.bat
 
-REM Verifie si le fichier main.py existe
+REM Vérifie si le fichier principal existe
 IF NOT EXIST "opening_downloader.py" (
     echo [ERREUR] Le script principal 'opening_downloader.py' est introuvable.
     goto End
 )
 
-CALL python opening_downloader.py
+REM Vérifie si 'python' est disponible
+where python >nul 2>nul
+IF %ERRORLEVEL%==0 (
+    echo [INFO] Exécution avec 'python'
+    CALL python opening_downloader.py
+) ELSE (
+    REM Vérifie si 'py' est disponible
+    where py >nul 2>nul
+    IF %ERRORLEVEL%==0 (
+        echo [INFO] Exécution avec 'py'
+        CALL py opening_downloader.py
+    ) ELSE (
+        echo [ERREUR] Ni 'python' ni 'py' n'ont été trouvés. Impossible d'exécuter le script.
+        goto End
+    )
+)
